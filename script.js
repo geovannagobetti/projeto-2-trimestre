794d99b8417e4745894efd040ac84aeb6b3b060a
Geovanna Gobetti <111057351+geovannagobetti@users.noreply.github.com>
2024-08-15 16:59:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,114 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");        
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "O que podemos fazer parar preservar o meio ambiente?",
-        alternativas: [
-            "Descartar resíduos em qualquer lugar",
-            "Descartar de forma apropriada e separar o lixo"
+         enunciado: "O que podemos fazer parar preservar o meio ambiente?",
+         alternativas: [
+            {
+                texto: "Descartar de forma apropriada e separar o lixo",
+                afirmacao: "correto",
+
+            },
+            {
+                texto: "Descartar resíduos em qualquer lugar",
+                afirmacao: "incorreta",
+            }
         ]
     },
     {
         enunciado: "Qual o impacto do desmatamento?",
         alternativas: [
-            "O desmatamento acaba com a diversidade e reduz o habitat dos animais",
-            "Essa ação contribui para a diversidade das espécies"
+            {
+                texto: "O desmatamento acaba com a diversidade e reduz o habitat dos animais",
+                afirmacao: "correto",
+            },
+            {
+                texto: "Essa ação contribui para a diversidade das espécies",
+                afirmacao: "incorreta",
+            }
         ]
-    }
-]; 
-const perguntas = [
+    },
     {
         enunciado: "Qual a principal causa do aquecimento global?",
         alternativas: [
-            "O aumento das emissões de gases de efeito estufa",
-            "Usar fontes de energia limpa e renovável"
+            {
+                texto: "O aumento das emissões de gases de efeito estufa",
+                afirmacao: "correta",
+            },
+            {
+                texto: "Usar fontes de energia limpa e renovável",
+                afirmacao: "incorreta",
+            }
         ]
     },
     {
         enunciado: "O que causa a chuva ácida?",
         alternativas: [
-            "Pela presença de poluentes atmosféricos",
-            "É causado por processos naturais, liberação de calor"
+            {
+                texto: "Pela presença de poluentes atmosféricos",
+                afirmacao: "correta",
+            },
+            {
+                texto: "É causado por processos naturais, liberação de calor",
+                afirmacao: "incorreta",
+            }
         ]
-    }
-]; 
-const perguntas = [
+    },
     {
         enunciado: "Como as atividades gumas impactam os oceanos?",
         alternativas: [
-            "Por resíduos plásticos e produtos químicos",
-            "Quando reciclamos o lixo dos mares"
+            { 
+                texto: "Por resíduos plásticos e produtos químicos",
+                afirmacao: "correta",
+            },
+            {
+                texto: "Quando reciclamos o lixo dos mares",
+                afirmacao: "incorreta",
+            }
         ]
     },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = 
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
